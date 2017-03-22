1dfcba5c6cd610b80e6bedfc780cd1a14375d456
云哥哥 <273461474@qq.com>
2017-03-23 02:17:10 +0800

    Create hello.lua

---
+++ src/hello.lua
@@ -0,0 +1,58 @@
+CC_USE_DEPRECATED_API = true
+require "cocos.init"
+local client = require "network.client"
+local LoginScene = require "LoginScene"
+
+-- cclog
+cclog = function(...)
+    print(string.format(...))
+end
+
+-- for CCLuaEngine traceback
+function __G__TRACKBACK__(msg)
+    cclog("----------------------------------------")
+    cclog("LUA ERROR: " .. tostring(msg) .. "\n")
+    cclog(debug.traceback())
+    cclog("----------------------------------------")
+end
+
+local function initGLView()
+    local director = cc.Director:getInstance()
+    local glView = director:getOpenGLView()
+    if nil == glView then
+        glView = cc.GLViewImpl:create("Lua Empty Test")
+        director:setOpenGLView(glView)
+    end
+
+    director:setOpenGLView(glView)
+
+    glView:setDesignResolutionSize(1280, 720, cc.ResolutionPolicy.NO_BORDER)
+
+    --turn on display FPS
+    director:setDisplayStats(true)
+
+    --set FPS. the default value is 1.0/60 if you don't call this
+    director:setAnimationInterval(1.0 / 60)
+end
+
+local function main()
+    -- avoid memory leak
+    collectgarbage("setpause", 100)
+    collectgarbage("setstepmul", 5000)
+
+    initGLView()
+
+    local visibleSize = cc.Director:getInstance():getVisibleSize()
+    local origin = cc.Director:getInstance():getVisibleOrigin()
+
+--	local c = client.new()
+--	c:connect("192.168.1.103", 8888)
+--	c:send("hello", {hello=1})
+--	c:close()
+	
+    -- run
+    local loginScene = LoginScene:create()
+    cc.Director:getInstance():runWithScene(loginScene)
+end
+
+xpcall(main, __G__TRACKBACK__)
